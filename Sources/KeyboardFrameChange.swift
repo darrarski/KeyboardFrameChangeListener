299c41e1b33798cfc54f5b994ea379c2b88a50b8
Dariusz Rybicki <darrarski@gmail.com>
2017-11-16 02:28:35 +0100

    Add public initialiser to KeyboardFrameChange struct

--- Sources/KeyboardFrameChange.swift
+++ Sources/KeyboardFrameChange.swift
@@ -3,8 +3,19 @@ import Foundation
 
 /// Represents keyboard frame change
 public struct KeyboardFrameChange {
+    /// Create new frame-change object
+    ///
+    /// - Parameters:
+    ///   - frame: new keyboard frame
+    ///   - animationDuration: change frame animation duration
+    public init(frame: CGRect, animationDuration: TimeInterval) {
+        self.frame = frame
+        self.animationDuration = animationDuration
+    }
+
     /// New keyboard frame
     public let frame: CGRect
-    /// Animation duration
+
+    /// Change frame animation duration
     public let animationDuration: TimeInterval
 }
